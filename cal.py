08e8ddc0cc2d5fa10210cae6517c18bf03d2b97a
Praneesha Rathnamalala <56305868+Praneesha-Irugal@users.noreply.github.com>
2022-06-07 07:19:10 +0000

    functions

---
+++ cal.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class Calculator:
+  def __init__(Self):
+    Self.window = tk.TK()
+    Self.window.geometry("375*667")
+    Self.window.resizable(0,0)
+    Self.window.title("Calculator")
